1abf352450fce0231994aad87c3f848109388b83
一点都不馋 <loseappetite@gmail.com>
2025-12-23 23:14:41 +0800

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>💖 Hello World! vue</h1>
-  <p>Welcome to your Electron application.</p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <p class=" text-9xl font-bold bg-red-500 text-white p-4">
+    你好，世界！
+  </p>
 </template>
 
 <script setup>
